404d66cfbe82aab89b5feae11da8e4c5bbfccc87
Mejerab <omim3670@gmail.com>
2024-12-20 17:03:38 +0600

    last

--- src/routes/route.jsx
+++ src/routes/route.jsx
@@ -22,7 +22,7 @@ const router = createBrowserRouter([
     {
         path: "/",
         element: <Main />,
-        // errorElement: <ErrorPage />,
+        errorElement: <ErrorPage />,
         children: [
             {
                 path: '/',
