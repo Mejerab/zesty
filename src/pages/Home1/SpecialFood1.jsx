bf78dfc25e1836901a1eb4ca768ae5395d85f766
Mejerab <omim3670@gmail.com>
2024-12-20 18:39:53 +0600

    new one

--- src/pages/Home1/SpecialFood1.jsx
+++ src/pages/Home1/SpecialFood1.jsx
@@ -6,19 +6,19 @@ const SpecialFood1 = () => {
                 <img src="https://i.ibb.co.com/CVwLBWh/Special-Foods.png" className="w-full lg:block hidden" alt="" />
                 <div className="grid gap-y-9 lg:grid-cols-4 justify-around w-full relative bottom-[7.5rem]">
                     <div data-aos='fade-right' className="stop-animation">
-                        <img className="spin" src="https://i.ibb.co.com/hK93Wcz/Image.png" alt="" />
+                        <img className="spin w-fit mx-auto" src="https://i.ibb.co.com/hK93Wcz/Image.png" alt="" />
                         <h4 className="corpo font-medium text-[32px] mt-5 text-center">Truffle Tango Pizza</h4>
                     </div>
                     <div data-aos='fade-down' className="stop-animation">
-                        <img className="spin" src="https://i.ibb.co.com/sjq6sZK/Image-1.png" alt="" />
+                        <img className="spin w-fit mx-auto" src="https://i.ibb.co.com/sjq6sZK/Image-1.png" alt="" />
                         <h4 className="corpo font-medium text-[32px] mt-5 text-center">Special Egg Fried</h4>
                     </div>
                     <div data-aos='fade-up' className="stop-animation">
-                        <img className="spin" src="https://i.ibb.co.com/hK93Wcz/Image.png" alt="" />
+                        <img className="spin w-fit mx-auto" src="https://i.ibb.co.com/hK93Wcz/Image.png" alt="" />
                         <h4 className="corpo font-medium text-[32px] mt-5 text-center">Spicy Supreme Pizza</h4>
                     </div>
                     <div data-aos='fade-left' className="stop-animation">
-                        <img className="spin" src="https://i.ibb.co.com/c82bGgv/Image-2.png" alt="" />
+                        <img className="spin w-fit mx-auto" src="https://i.ibb.co.com/c82bGgv/Image-2.png" alt="" />
                         <h4 className="corpo font-medium text-[32px] mt-5 text-center">Chicken Salad Seeds</h4>
                     </div>
                 </div>
